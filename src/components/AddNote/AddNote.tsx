2c6de6c5e2dfeab352401b31e2822ecaa5019cb6
SemenShOl <semen.kursk.ru@gmail.com>
2024-04-06 19:03:50 +0300

    before delete animation

--- src/components/AddNote/AddNote.tsx
+++ src/components/AddNote/AddNote.tsx
@@ -1,17 +1,22 @@
 import React, { FC } from "react";
-import { IoMdAdd } from "react-icons/io";
-import { FaCircle } from "react-icons/fa";
 import cl from "./AddNote.module.scss";
 import cn from "classnames";
 import { AddNoteProps } from "./AddNoteProps";
-
-export const AddNote: FC<AddNoteProps> = ({ size, className, onAddNote }) => {
+import { motion } from "framer-motion";
+import { ICPlus } from "../../icons";
+export const AddNote: FC<AddNoteProps> = ({ className, onAddNote }) => {
   return (
     <div className={cn(cl.wrapper, className)} onClick={onAddNote}>
-      <div className={cl.addIcon}>
-        <IoMdAdd size={35} className={cl.plus} />
-        <FaCircle size={45} className={cl.circle} />
-      </div>
+      <motion.div
+        className={cl.addIcon}
+        whileHover={{
+          scale: 1.05,
+          transition: { duration: 0.2, ease: "easeInOut" },
+        }}
+      >
+        {/* <IoMdAdd size={35} className={cl.plus} /> */}
+        <ICPlus className={cl.plus} />
+      </motion.div>
     </div>
   );
 };
